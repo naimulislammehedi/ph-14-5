97d352a239006bbf402bdcb567e2a564bc9f8569
Mehedi <mehedi.interface@gmail.com>
2026-04-16 21:08:37 +0600

    14_5-3 Array length, index, Get and set by index

---
+++ length.js
@@ -0,0 +1,11 @@
+const numbers = [1, 4, 66, 11, 45, 65]; 
+
+console.log(numbers.length);
+
+// check index of an array
+console.log(numbers[3]); 
+const fourth = numbers[3];
+console.log(fourth);
+
+numbers[1] = 55;
+console.log(numbers);
