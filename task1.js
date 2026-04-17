62d39e41dd8e35b90cd550e8231ed991313dc85f
Mehedi <mehedi.interface@gmail.com>
2026-04-17 08:48:03 +0600

    task 1

---
+++ task1.js
@@ -0,0 +1,14 @@
+/*
+1. Declare an array
+- Declare an array with 5 elements containing fruits
+- console log the 3rd index element
+- change the value of the 2nd index element to jambura
+- console log the final array
+*/
+
+let fruits = ["apple", "banana", "orange", "grape", "mango"];
+
+console.log(fruits[3]); 
+
+fruits[2] = "jambura"; 
+console.log(fruits); 
