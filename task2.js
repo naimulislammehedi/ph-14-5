894a8ee6c3810c9fc37d56996945e3af505408d8
Mehedi <mehedi.interface@gmail.com>
2026-04-17 09:05:29 +0600

    task 2

---
+++ task2.js
@@ -0,0 +1,18 @@
+/*
+Add or remove elements
+- Declare an array of 3 tourist destinations
+- Add a new tourist destination to your tourist array
+- Add two more to your array
+- Remove the last tourist destination you have added
+- display the final array as output
+*/
+
+let touristDestinations = ["Paris", "Tokyo", "New York"];
+
+touristDestinations.push("Cox's Bazar"); 
+touristDestinations.push("Great Wall", "Venice"); 
+
+// console.log(touristDestinations); 
+
+touristDestinations.pop(); 
+console.log(touristDestinations); 
